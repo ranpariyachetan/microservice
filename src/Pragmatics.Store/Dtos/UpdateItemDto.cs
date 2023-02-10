1d8d89a9382c01f653a62fa2a375d5250b86e0ae
Chetan Ranpariya <ranpariyachetan.ad@gmail.com>
2023-02-10 16:06:30 +0800

    Added annotation on Name and Price properties of Dtos.

--- src/Pragmatics.Store/Dtos/UpdateItemDto.cs
+++ src/Pragmatics.Store/Dtos/UpdateItemDto.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Pragmatics.Store.Dtos
 {
     public class UpdateItemDto
     {
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [Range(0, 1000)]
         public double Price { get; set; }
     }
 }
