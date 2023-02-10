98acdda0a92c6b5d4c234ad51c292b3f735a7e54
Chetan Ranpariya <ranpariyachetan.ad@gmail.com>
2023-02-10 14:49:08 +0800

    Added GET endpoints for items.

--- src/Pragmatics.Store/Controllers/ItemsController.cs
+++ src/Pragmatics.Store/Controllers/ItemsController.cs
@@ -1,8 +1,35 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Pragmatics.Store.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Pragmatics.Store.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ItemsController : ControllerBase
     {
+        private static List<ItemDto> dataStore = new List<ItemDto>
+        {
+            new ItemDto {ID = Guid.NewGuid(), Name = "Cold Drink", Price = 2.0},
+            new ItemDto {ID = Guid.NewGuid(), Name = "Ice Cream", Price = 1.80},
+            new ItemDto {ID = Guid.NewGuid(), Name = "Chocolate", Price = 1.20},
+            new ItemDto {ID = Guid.NewGuid(), Name = "Fruit Juice", Price = 2.0}
+        };
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<ItemDto> Get()
+        {
+            return dataStore;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ItemDto GetById(Guid id)
+        {
+            return dataStore.SingleOrDefault(item => item.ID == id);
+        }
     }
 }
--- src/Pragmatics.Store/Dtos/ItemDto.cs
+++ src/Pragmatics.Store/Dtos/ItemDto.cs
@@ -2,7 +2,7 @@ namespace Pragmatics.Store.Dtos
 {
     public class ItemDto
     {
-        public Guid Id {get;set;}
+        public Guid ID {get;set;}
 
         public string Name { get; set; }
 
